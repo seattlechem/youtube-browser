380b43dfe24d72a653db0035e2fa0e144411aab3
seattlechem <seattlechem@yahoo.com>
2018-07-27 22:37:34 -0700

    implement YTSearch

--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
 // import './index.css';
 // import App from './App';
 // import registerServiceWorker from './registerServiceWorker';
 
 const API_Key = 'AIzaSyDoChwaCR7ezbYQeMRGdy12lPS2Ok72uOk';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  )
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {videos: []};
+    YTSearch({key: API_Key, term: 'korea'}, (data) => {
+      this.setState({videos: data});
+    });
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }  
 }
 ReactDOM.render(<App />, document.getElementById('root'));
 // registerServiceWorker();
