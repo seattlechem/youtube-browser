9297880e44a88044a42078e58b61447630c3d41d
Peter Kim <seattlechem@yahoo.com>
2018-07-30 11:22:26 -0700

    YTSearch imported from youtube-api-search

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
 // import './index.css';
 // import App from './App';
 // import registerServiceWorker from './registerServiceWorker';
