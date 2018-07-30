9281c3dbf2fc36a6f823fdd9236003e8c07e95fe
Peter Kim <seattlechem@yahoo.com>
2018-07-30 11:44:24 -0700

    app refactored to class componenet

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
@@ -8,12 +8,22 @@ import YTSearch from 'youtube-api-search';
 
 const API_KEY = process.env.REACT_APP_YOUTUBE_KEY
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  )
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {videos: []};
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (data) => {
+      this.setState({videos: data});
+    });
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  } 
 }
 ReactDOM.render(<App />, document.getElementById('root'));
 // registerServiceWorker();
