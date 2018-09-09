ddb380b8b0b23a207544428a1aa38d4e9a3d6bed
Peter Kim <seattlechem@yahoo.com>
2018-09-09 10:33:47 -0700

    selectedVideo added in index.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,10 @@ const API_KEY = process.env.REACT_APP_YOUTUBE_KEY
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {videos: []};
+    this.state = {
+      videos: [],
+      selectedVideo = null
+    };
 
     YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
       this.setState({videos});
@@ -23,7 +26,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoDetail video={this.state.videos[0]} />
+        <VideoDetail video={this.state.selectedVideo} />
         <VideoList videos={this.state.videos} />
       </div>
     );
